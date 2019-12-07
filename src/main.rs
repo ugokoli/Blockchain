7868ec6ba0a656a2fb9193c1e27a40288a4a812b
Ugonna Okoli <ugokoli30@gmail.com>
2019-12-07 12:42:45 +0100

    Block scaffold

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use blockchainlib::*;
+
+fn main() {
+    let difficulty = 0x0000ffffffffffffffffffffffffffff;
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned(), difficulty);
+
+    block.mine();
+    println!("Mined genesis block {:?}", &block);
+
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    for i in 1..=10 {
+        let mut block = Block::new(i, 0, last_hash, 0, "Another block".to_owned(), difficulty);
+
+        block.mine();
+        println!("Mined block {:?}", &block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block)
+    }
+}
